3c52491d74351ed9fe5d1a53ca23412b51efc99f
Waimea-OBrownrigg <ocbrownrigg@waimea.school.nz>
2025-02-18 15:07:40 +1300

    Making the title

---
+++ src/Main.kt
@@ -0,0 +1,8 @@
+fun main() {
+    println("                ===================================Generic_Studios Presents,===================================")
+    println("====================A Perfectly Ordinary calculator, Yup, Nothing at all unusual about this here Calculator====================")
+    println("====================        In Fact, I Doubt You Could Find A More Normal Calculator Than This One!        ====================")
+    println("                ===============================================================================================")
+
+
+}
